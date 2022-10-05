248016dca9133a82ea6d0334a8e6e4e5f4ef3e0a
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-10-05 22:05:35 +0500

    TEACH Ternary operator

--- Program.cs
+++ Program.cs
@@ -10,13 +10,38 @@
         Console.WriteLine($"Successfully converted!  {age} ");
         int keschasAge = 2;
         int ageDifference = age - keschasAge;
+        // if(age >  keschasAge){
+        //       System.Console.WriteLine($"You are older KESCHA");  
+        // }
+        // else  if( age == keschasAge ){
+        //   System.Console.WriteLine("You are equal");
+        // }
+        // else{
+        //         System.Console.WriteLine("You are younger ");
+        // }
+        // if(age < keschasAge ){
+        //         System.Console.WriteLine("Siz keshandan yosh kichiksiz");
+        // }
+        // else if (age == keschasAge ){
+        //         System.Console.WriteLine("Sizning yoshingiz teng");
+        // }
+        // else {
+        //         System.Console.WriteLine("Siz Keshadan yosh kattasiz");     
+        // }
+        string malumot = 
+                age >= keschasAge 
+                        ? "Siz keshandan yosh kattasiz"
+                        : "Siz keshandan yosh kichiksiz";
+
+        System.Console.WriteLine(malumot );
+
         Console.WriteLine($"The difference between your and Kescha`s age is {ageDifference}");
-        System.Console.WriteLine($"You are older KESCHA :  {ageDifference > 0}");
-         System.Console.WriteLine($"You are older KESCHA :  {age >  keschasAge}");
-         System.Console.WriteLine($"You are younger KESCHA :  {age < keschasAge}");
-         System.Console.WriteLine($"You are older  that equal to KESCHA :  {age >=  keschasAge}");
-         System.Console.WriteLine($"You are younger thet equal to KESCHA :  {age <= keschasAge}"); 
-         System.Console.WriteLine($"You are the same age as KESCHA:  {age == keschasAge}");
-         System.Console.WriteLine($"You are not the same age as KESCHA:  {age != keschasAge}");
+        // System.Console.WriteLine($"You are older KESCHA :  {ageDifference > 0}");
+        //  System.Console.WriteLine($"You are older KESCHA :  {age >  keschasAge}");
+        //  System.Console.WriteLine($"You are younger KESCHA :  {age < keschasAge}");
+        //  System.Console.WriteLine($"You are older  that equal to KESCHA :  {age >=  keschasAge}");
+        //  System.Console.WriteLine($"You are younger thet equal to KESCHA :  {age <= keschasAge}"); 
+        //  System.Console.WriteLine($"You are the same age as KESCHA:  {age == keschasAge}");
+        //  System.Console.WriteLine($"You are not the same age as KESCHA:  {age != keschasAge}");
 
 
